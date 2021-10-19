c3cfdda1cec5971e72df274575eb29288629258b
Hitesh <imhr1727@gmail.com>
2021-10-19 11:49:12 +0300

    Initial commit

---
+++ app/src/main/java/com/rtu/welearn/activity/DashboardActivity.kt
@@ -0,0 +1,14 @@
+package com.rtu.welearn.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.rtu.welearn.R
+
+class DashboardActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+
+}
